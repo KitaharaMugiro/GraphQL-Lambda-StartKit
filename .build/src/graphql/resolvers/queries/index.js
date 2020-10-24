4d6a5498ab72a399d376b5b88037e56b1933e1bf
KitaharaMugiro <you@example.com>
2020-10-24 16:19:07 +0900

    test

--- .build/src/graphql/resolvers/queries/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var dummyQuery_1 = require("./dummyQuery");
-exports.dummyQuery = dummyQuery_1.default;
-//# sourceMappingURL=index.js.map
--- .build/src/handler.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var apolloServer_1 = require("./graphql/apolloServer");
-exports.graphqlHandler = apolloServer_1.default;
-//# sourceMappingURL=handler.js.map
